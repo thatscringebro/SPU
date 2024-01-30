b9970e65dbe638bd720285a93fd3651d523817c6
Gabriel Bruneau <1974278@etu.cegepsth.qc.ca>
2024-01-30 17:43:56 -0500

    modification de la façon de faire pour afficher les plages horaires

--- ViewComponents/ScheduleDay.cs
+++ ViewComponents/ScheduleDay.cs
@@ -6,18 +6,10 @@ namespace SPU.ViewComponents
     {
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            int Annee = DateTime.Now.Year;
-            List<DateTime> Jours = new List<DateTime>();
-            for (int mois = 1; mois <= 12; mois++)
-            {
-                for (int jour = 1; jour <= DateTime.DaysInMonth(Annee, mois); jour++)
-                {
-                    Jours.Add(new DateTime(Annee, mois, jour));
-                }
-            }
+
 
             // Renvoyer la vue avec les données
-            return View(Jours);
+            return View();
         }
 
     }
