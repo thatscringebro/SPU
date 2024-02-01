3b4d5643f01d4cfd0644e72242d6e3579cf49be7
Gabriel Bruneau <1974278@etu.cegepsth.qc.ca>
2024-02-01 16:22:57 -0500

    Horaire affichage des heures travaillés

--- Controllers/HoraireController.cs
+++ Controllers/HoraireController.cs
@@ -6,6 +6,7 @@ namespace SPU.Controllers
     {
         public IActionResult Index()
         {
+
             return View();
         }
     }
--- ViewModels/JourneeTravailleVM.cs
+++ ViewModels/JourneeTravailleVM.cs
@@ -2,6 +2,7 @@
 {
     public class JourneeTravailleVM
     {
+        public Guid Id { get; set; }
         public DateTime DateDebutQuart { get; set; }
         public DateTime DateFinQuart { get; set; }
         public bool Present { get; set; } = true;
