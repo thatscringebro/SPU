275aca2c99587dbd54c596d9fb646c3d49b2913f
FrancoisLampron <francois.lampron@hotmail.com>
2024-02-05 16:40:07 -0500

    Ajout plage horaire formulaire

---
+++ ViewModels/PlageHoraireMdsVM.cs
@@ -0,0 +1,13 @@
+﻿namespace SPU.ViewModels
+{
+    public class PlageHoraireMdsVM
+    {
+        public Guid id { get; set; }
+        public int ChoixSemaine { get; set; }
+        public int ChoixJournee { get; set; } // 1 = Lundi 2 = Mardi ..
+        public string HeureDebut { get; set; }
+        public string HeureFin { get; set; }
+        public DateTime DateDebutStage { get; set; }
+        public DateTime DateFinStage { get; set; }
+    }
+}
