6fc4b879cde2b6b3fc9e2f55fa8ff8cdf860ffc2
Claudel-D-Roy <112507354+Claudel-D-Roy@users.noreply.github.com>
2024-02-05 17:22:47 -0500

    Editeur d'utilisateur

---
+++ ViewModels/UtilisateurEditVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SPU.ViewModels
+{
+    public class UtilisateurEditVM
+    {
+        public Guid id { get; set; }
+        [Display(Name = "Nom d'utilisateur")]
+        public string userName { get; set; }
+        [Display(Name = "Prénom")]
+        public string Prenom { get; set; }
+        [Display(Name = "Nom")]
+        public string Nom { get; set; }
+        [Display(Name = "Numéro de cellulaire")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Mot de passe")]
+        public string pwd { get; set; }
+        [Display(Name = "Confirmation mot de passe")]
+        public string confirmationpwd { get; set; }
+    }
+}
